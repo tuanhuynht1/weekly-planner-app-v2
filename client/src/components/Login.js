ff89afeec38a057f4c2f09a960f3e49d81ab436e
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-05-01 15:19:20 -0400

    crude login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,7 +25,26 @@ const Login = () => {
             cookie.save('token',res.data);
             window.location = '/';
         })
-        .catch( e => console.error(e.response));
+        .catch( e => setMssg('Invalid Credentials'));
+    }
+
+    const signup = (e) => {
+        e.preventDefault();
+        if(usr.trim() === '' || pwd.trim() === ''){
+            setMssg('All fields must be filled');
+            return;
+        }
+        // call login api to recieve access token
+        axios
+        .post('api/auth/register',{
+            usr: usr,
+            pwd: pwd
+        })
+        .then(res => {
+            // gets authenticated token
+            window.location = '/';
+        })
+        .catch( e => setMssg('Invalid Credentials'));
     }
 
 
@@ -38,6 +57,12 @@ const Login = () => {
                 <button type='submit'>sign in</button>
             </form>
             <p>{mssg}</p>
+            <h1>Signup</h1>
+            <form className='login-form' onSubmit={e => signup(e)}>
+                <input type='text' onChange={e => setUsr(e.target.value)}/>
+                <input type='password' onChange={e => setPwd(e.target.value)}/>
+                <button type='submit'>sign up</button>
+            </form>
         </Fragment>
     )
 }
